63dcba4603360a93c93f56d7015dcf332a0aa026
horace19 <1019horacefung@gmail.com>
2025-01-21 22:12:57 +0800

    Add basic layout of the project Add super simple and rough requirment of the code as comment in script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello world?");
+/*Computer randomly return "rock", "paper" or "scissors"
+function getComputerChoice
+use prompt to get user input getHumanChoice (case not sensitive)
+humanScore computerScore
+round winner announcement
+“You lose! Paper beats Rock”
+playRound as the function of single round
+playGame call playRound 5 times
+
