b4added7ef6abf7c229e6dd20a8ad356f7d1fc56
horace19 <1019horacefung@gmail.com>
2025-01-22 09:53:19 +0800

    Add pseudocode of the game in script.js

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ round winner announcement
 playRound as the function of single round
 playGame call playRound 5 times
 
+let computerChoice
+let randomComputerNum
+let humanChoice
+let computerScore = 0
+let humanScore = 0
+
+FUNCTION getComputerChoice()
+    randomComputerNum = Round down (random number between 0-1 then times 3 plus 1)
+    SWITCH(randomComputerNum)
+        Case 1 rock 2 paper 3 scissors
+
+FUNCTION getHumanChoice()
+    humanChoice = toLowerCase (Prompt "Please enter your choice")
+    IF humanChoice != rock || humanChoice != paper || humanChoice != scissors
+        print invalid
+
+FUNCTION playRound()
+
+    IF humanChoice == computerChoice
+        print "draw"
+
+    ELSE IF human and computer => rock $$ scissors || paper $$ rock || scissors $$ paper
+        humanScore ++
+        Print "You win! humanChoice beats computerChoice"
+
+    ELSE
+        computerScore ++
+        print "You lost! computerChoice beat humanChoice"
+
+
+
+*/
